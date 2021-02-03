5324f5ea21652aaee6c883849950664d513f1bd0
Zav <zav@cherre.com>
2021-02-03 15:07:46 -0500

    rebuild Wed Feb  3 15:07:46 EST 2021

--- parse_commit_args.js
+++ parse_commit_args.js
@@ -11,9 +11,9 @@ class Output {
 }
 
 async function get_head_commit() {
-//   console.log(JSON.stringify(github.context.payload.head_commit, null, 2))
+  //   console.log(JSON.stringify(github.context.payload.head_commit, null, 2))
   if (github.context.payload.head_commit != null)
-    return { ...github.context.payload.head_commit }
+    return Object.assign({}, github.context.payload.head_commit)
 
   let commits_url = github.context.repository.commits_url
 
